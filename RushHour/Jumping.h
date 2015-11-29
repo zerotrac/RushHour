39b46ef21016bc036692cd6eb2f38e50a556b326
Chen Shuxin <734688610@qq.com>
2015-11-29 12:50:14 +0800

    RushHour

---
+++ RushHour/Jumping.h
@@ -0,0 +1,9 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+
+const int Gravity = 2;
+
+using namespace std;
+
--- RushHour/RushHour/Jumping.h
+++
@@ -1,9 +0,0 @@
-#include <cstdio>
-#include <cstdlib>
-#include <cmath>
-#include <algorithm>
-
-const int Gravity = 2;
-
-using namespace std;
-
